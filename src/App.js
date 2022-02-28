26e56ad3a0607c50b3fc0223f4ee054f18ca2ab9
Nico Picchio <nicolapicchio@gmail.com>
2022-02-28 12:15:21 +0000

    extension 2 completed

--- src/App.js
+++ src/App.js
@@ -5,37 +5,49 @@ import './App.css';
 function App() {
 	const [workouts, setWorkouts] = useState(initialWorkouts);
 
-	const addNewWorkout = () => {	setWorkouts([...workouts, generateWorkout()])	};
+	const addNewWorkout = () => {
+		setWorkouts([...workouts, generateWorkout()]);
+	};
 
-	const deleteWorkout = (targetWorkout) => {	setWorkouts(workouts.filter((workout) => workout !== targetWorkout)) };
+	const deleteWorkout = (targetWorkout) => {
+		setWorkouts(workouts.filter((workout) => workout !== targetWorkout));
+	};
 
-	const completeWorkout = (targetWorkout) => { 
-    const updatedWorkout = workouts.map(workout => {
-      if (workout === targetWorkout) {
-        return { ...workout, done: true };
-      }
-      return workout;
-    })
-    setWorkouts(updatedWorkout);
-  };
+	const completeWorkout = (targetWorkout) => {
+		const updatedWorkout = workouts.map((workout) => {
+			if (workout === targetWorkout) {
+				return { ...workout, done: true };
+			}
+			return workout;
+		});
+		setWorkouts(updatedWorkout);
+	};
 
-  const replaceWorkout = (targetWorkout) => {
-    if (!targetWorkout.done) {
-      deleteWorkout(targetWorkout);
-    }
-    addNewWorkout()
-  }
+	const replaceWorkout = (targetWorkout) => {
+		const newlyGeneratedWorkout = generateWorkout()
+		setWorkouts(workouts.map((workout) => {
+			if (workout === targetWorkout) {
+				return newlyGeneratedWorkout
+			} else return workout
+		}));
+	};
 
 	return (
 		<div className="App">
 			<h1>🏋️‍♀️Workout Generator</h1>
-      <div className="top-btns">
-        <button id="add-workout-btn" onClick={addNewWorkout}>Add New Workout</button>
-        <div id="checkbox-completed">
-          <input type="checkbox" id="showCompleted" name="showCompleted"></input>
-          <label for="showCompleted">Show completed workouts</label>
-        </div>
-      </div>
+			<div className="top-btns">
+				<button id="add-workout-btn" onClick={addNewWorkout}>
+					Add New Workout
+				</button>
+				<div id="checkbox-completed">
+					<input
+						type="checkbox"
+						id="showCompleted"
+						name="showCompleted"
+					></input>
+					<label for="showCompleted">Show completed workouts</label>
+				</div>
+			</div>
 			<ul>
 				{workouts.map((workout, index) => (
 					<li key={index}>
@@ -51,7 +63,11 @@ function App() {
 						)}
 						{workout.done && <p>✅</p>}
 						<button onClick={() => deleteWorkout(workout)}>Delete</button>
-            {!workout.done && (<button onClick={() => replaceWorkout(workout)}>Replace {workout.exercise}</button>)}
+						{!workout.done && (
+							<button onClick={() => replaceWorkout(workout)}>
+								Replace {workout.exercise}
+							</button>
+						)}
 					</li>
 				))}
 			</ul>
